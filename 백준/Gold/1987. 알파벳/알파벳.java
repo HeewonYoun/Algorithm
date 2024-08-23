cbd142dc39d29dd2ea45ff862aefca32c99dd32f
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2024-08-23 15:04:54 +0900

    [Gold IV] Title: 알파벳, Time: 2072 ms, Memory: 296592 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,57 +1,64 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] dx = { -1, 0, 1, 0 }; // 상우하좌
-	static int[] dy = { 0, 1, 0, -1 };
-	static boolean[] check = new boolean[26];
-	static int max = Integer.MIN_VALUE;
-	static char[][] board;
-	static int r,c;
+    static int[] dx = {-1, 0, 1, 0}; //상우하좌
+    static int[] dy = {0, 1, 0, -1};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    static int R, C;
+    static char[][] map;
+    static int result = Integer.MIN_VALUE;
+    static HashMap<Character, Integer> alpha = new HashMap<>();
+    static boolean[][] visited;
 
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		board = new char[r][c];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		for (int i = 0; i < r; i++) {
-			char[] tmp = br.readLine().toCharArray();
-			for (int j = 0; j < c; j++) {
-				board[i][j] = tmp[j];
-			}
-		}
-		//ABCDE FGHIJ KLMNO PQRST UVWXY Z
+        R = Integer.parseInt(st.nextToken()); //행
+        C = Integer.parseInt(st.nextToken()); //열
 
-		// 1행 1열 - 말
-		dfs(0, 0, 1);
-		
-		System.out.println(max);
-	}
+        map = new char[R][C];
+        visited = new boolean[R][C];
 
-	static void dfs(int x, int y, int cnt) {
-		// 같은 알파벳 적힌 칸 이면 return
-		if (check[board[x][y] - 65]) {
-			if (cnt-1 > max) {
-				max = cnt-1;
-			}
-			return;
-		}
+        for(int i = 0; i<R; i++){
+            char[] tmp = br.readLine().toCharArray();
+            for(int j = 0; j<C; j++){
+                map[i][j] = tmp[j];
+            }
+        }
 
-		int nx, ny;
-		check[board[x][y] - 65] = true;
-		for (int i = 0; i < 4; i++) {
-			nx = x + dx[i];
-			ny = y + dy[i];
-			
-			if(nx < 0 || nx >= r || ny < 0 || ny >=c) continue;
+        visited[0][0] = true;
+        alpha.put(map[0][0], 1);
+        dfs(0,0, 1);
 
-			dfs(nx, ny, cnt+1);
+        System.out.println(result);
+    }
 
-		}
-		check[board[x][y] - 65] = false;
-	}
+    static void dfs(int sx, int sy, int cnt){
+        if(cnt > result){
+            result = cnt;
+        }
+
+        //반복 & 원상복구
+        for(int i = 0; i<4; i++){
+            int nx = sx + dx[i];
+            int ny = sy + dy[i];
+
+            if(nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+            if(visited[nx][ny]) continue;
+            if(alpha.containsKey(map[nx][ny])) continue;
+
+            visited[nx][ny] = true;
+            alpha.put(map[nx][ny] , 1);
+            dfs(nx, ny, cnt + 1);
+
+            visited[nx][ny] = false;
+            alpha.remove(map[nx][ny]);
+        }
+    }
 }
