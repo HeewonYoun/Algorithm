861dfe383c484a5a580fe1afb5f279e22b90cb72
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2024-09-27 19:17:47 +0900

    [Silver III] Title: 과제는 끝나지 않아!, Time: 920 ms, Memory: 222856 KB -BaekjoonHub

--- 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
+++ 백준/Silver/17952. 과제는 끝나지 않아！/과제는 끝나지 않아！.java
@@ -1,37 +1,45 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); //이번학기 몇 분인지
+        Stack<int[]> task = new Stack<>();
+        int result = 0;
 
-		int N = Integer.parseInt(br.readLine());
-		Stack<int[]> s = new Stack<>();
-		int result = 0;
-		
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			int check = Integer.parseInt(st.nextToken());
-			if(check == 1) {
-				int A = Integer.parseInt(st.nextToken());
-				int T = Integer.parseInt(st.nextToken());
-				s.push(new int[] {A, T});
-			}
-			
-			if(!s.isEmpty()) {
-				int[] tmp = s.pop();
-				
-				tmp[1]--;
-				if(tmp[1] == 0) {
-					result += tmp[0];
-				}else {
-					s.push(tmp);
-				}
-			}
-		}
-		
-		System.out.println(result);
-	}
+        for(int i = 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+
+            if(num == 1){
+                int A = Integer.parseInt(st.nextToken()); //과제 만점
+                int T = Integer.parseInt(st.nextToken()); //걸리는 시간
+
+                if(T == 1){
+                    result += A;
+                } else {
+                    task.add(new int[]{A, T-1});
+                }
+
+            } else if(num == 0 && !task.isEmpty()){
+                int[] cur = task.pop();
+
+                if(cur[1] == 1) {
+                    cur[1] = 0;
+                    result += cur[0];
+
+                } else {
+                    task.add(new int[]{cur[0], cur[1]-1});
+                }
+            }
+        }
+        System.out.println(result);
+
+    }
 }
