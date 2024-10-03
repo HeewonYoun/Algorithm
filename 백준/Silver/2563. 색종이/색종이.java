485892f5bfb2a0a7dc46527161261f85e761993d
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2024-10-03 21:48:17 +0900

    [Silver V] Title: 색종이, Time: 68 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,38 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); //색종이 수
+        int[][] bg = new int[101][101];
 
-		int n = Integer.parseInt(br.readLine()); // 색종이 수
-		int size = 0; // 색종이 붙은 검은 영역의 넓이
-		int[][] board = new int[101][101];
-		int sx, sy;
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			sx = Integer.parseInt(st.nextToken());
-			sy = Integer.parseInt(st.nextToken());
+            //3,7 ~ 13,17
+            for(int j = 1; j<11; j++){
+                for(int k = 1; k<11; k++){
+                    if(bg[x+j][y+k]==0) bg[x+j][y+k]= 1;
+                }
+            }
 
-			for (int j = sx; j < sx + 10; j++) {
-				for (int k = sy; k < sy + 10; k++) {
-					if(board[j][k] == 0) {
-						board[j][k] = 1;
-					}
-				}
-			}
-		}
+        }
 
-		for(int i = 0; i<101; i++) {
-			for(int j = 0; j<101; j++) {
-				if(board[i][j] == 1) {
-					size++;
-				}
-			}
-		}
-		System.out.println(size);
-	}
+        //검은 영역 넓이
+        int result = 0;
+        for(int i = 1; i<101; i++){
+            for(int j = 1; j<101; j++){
+                if(bg[i][j] == 1) result++;
+            }
+        }
 
+        System.out.println(result);
+    }
 }
