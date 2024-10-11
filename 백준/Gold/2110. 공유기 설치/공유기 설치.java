b1ae09d8d4901e06691332ad85c68a4504f76f1b
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2024-10-11 22:38:40 +0900

    [Gold IV] Title: 공유기 설치, Time: 224 ms, Memory: 28236 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -6,41 +6,39 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int C;
+    static int N, C;
     static int[] house;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); //집의 개수 (2<=N<=20만)
-        C = Integer.parseInt(st.nextToken()); //공유기 개수 (2<=C<=N)
+        N = Integer.parseInt(st.nextToken()); //집의 개수
+        C = Integer.parseInt(st.nextToken()); //공유기 개수
 
         house = new int[N];
-
         for(int i = 0; i<N; i++){
-            house[i] = Integer.parseInt(br.readLine());
+            house[i] = Integer.parseInt(br.readLine()); //집 좌표
         }
 
-        Arrays.sort(house);
+        //이분탐색
+        Arrays.sort(house); //정렬
 
-        int lo = 1; // 최솟값
-        int hi = house[N-1] - house[0]+1; // 최대 거리
+        int low = 1;
+        int high = house[N-1] - house[0] + 1;
         int mid = 0;
 
-        while(lo < hi){
-            mid = (lo + hi) / 2;
+        while(low < high){
+            mid = (low + high) / 2;
 
             if(canInstall(mid) < C) { // 설치 가능한 공유기 개수가 C보다 작으면
-                hi = mid; //거리를 좁힘
+                high = mid; //거리 좁힘
             } else {
-                lo = mid + 1; //거리를 늘림
+                low = mid + 1; //거리 늘림
             }
         }
 
-        System.out.println(lo-1);
-
+        System.out.println(low-1);
     }
 
     static int canInstall(int dist){
