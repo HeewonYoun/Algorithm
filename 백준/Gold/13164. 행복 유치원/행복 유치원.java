7f6ba78b8be84d34a76e488adcd0e6b0206fd56c
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2024-11-19 17:03:59 +0900

    [Gold V] Title: 행복 유치원, Time: 388 ms, Memory: 61736 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -2,13 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    
+    static int N, K;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); //원생 수
-        int K = Integer.parseInt(st.nextToken()); //조의 개수
+        N = Integer.parseInt(st.nextToken()); //원생 수
+        K = Integer.parseInt(st.nextToken()); //조의 개수
 
         int[] height = new int[N];
         st = new StringTokenizer(br.readLine());
@@ -16,15 +18,12 @@ public class Main {
             height[i] = Integer.parseInt(st.nextToken());
         }
 
-        //티셔츠 비용 = 가장 키 큰 원생 - 가장 키 작은 원생
-        //K개 조 티셔츠 만드는 비용의 합 최소
         int[] diff = new int[N-1];
         for(int i = 1; i<N; i++){
             diff[i-1] = height[i] - height[i-1];
         }
-
-        //키 차이 오름차순 정렬
-        Arrays.sort(diff);
+        
+        Arrays.sort(diff); //키 차이 오름차순 정렬
         int result = 0;
         for(int i = 0; i<N-K; i++){
             result += diff[i];
