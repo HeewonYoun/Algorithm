33f5949712cd5bc5ce073cb992cd25b22b94703a
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2024-12-24 14:49:39 +0900

    [Gold IV] Title: 문제 추천 시스템 Version 1, Time: 504 ms, Memory: 53452 KB -BaekjoonHub

--- 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
+++ 백준/Gold/21939. 문제 추천 시스템 Version 1/문제 추천 시스템 Version 1.java
@@ -3,99 +3,78 @@ import java.util.*;
 
 public class Main {
 
-    static PriorityQueue<int[]> pqMin;
-    static PriorityQueue<int[]> pqMax;
-    static Map<Integer, Integer> map;
+    static int N;
+
+    public static class Problem implements Comparable<Problem> {
+        int number;
+        int level;
+
+        Problem(int number, int level) {
+            this.number = number;
+            this.level = level;
+        }
+
+        @Override
+        public int compareTo(Problem o) {
+            if (this.level == o.level) {
+                return Integer.compare(this.number, o.number);
+            } else {
+                return Integer.compare(this.level, o.level);
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
 
-        //쉬운 문제 - 번호 작은순으로 출력
-        //어려운 문제 여러개 - 문제 번호 큰 순으로 출력
-        pqMin = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1]){
-                    return o1[0] - o2[0];
+        N = Integer.parseInt(br.readLine());
+        TreeSet<Problem> problemSet = new TreeSet<>(); //문제를 관리
+        Map<Integer, Integer> problemMap = new HashMap<>(); //문제 번호로 난이도 관리
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken()); //문제 번호
+            int l = Integer.parseInt(st.nextToken()); //난이도
+
+            problemSet.add(new Problem(p, l));
+            problemMap.put(p, l);
+        }
+
+        int M = Integer.parseInt(br.readLine()); // 명령문 개수
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            if (command.equals("add")) {
+                int p = Integer.parseInt(st.nextToken());
+                int l = Integer.parseInt(st.nextToken());
+
+                problemSet.add(new Problem(p, l));
+                problemMap.put(p, l);
+
+            } else if (command.equals("recommend")) {
+                int x = Integer.parseInt(st.nextToken());
+                if (x == 1) {
+                    sb.append(problemSet.last().number).append("\n"); //난이도 가장 높은 문제
+
+                } else if (x == -1) {
+                    sb.append(problemSet.first().number).append("\n"); //난이도 가장 낮은 문제
                 }
-                return o1[1] - o2[1];
-            }
-        });
-        pqMax = new PriorityQueue<>(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1]){
-                    return o2[0] - o1[0];
-                }
-                return o2[1] - o1[1];
-            }
-        });
-        map = new HashMap<>();
 
-        for(int i = 0; i<N; i++){
-            st = new StringTokenizer(br.readLine().trim());
-            int P = Integer.parseInt(st.nextToken()); //문제번호
-            int L = Integer.parseInt(st.nextToken()); //난이도
+            } else if (command.equals("solved")) {
+                int p = Integer.parseInt(st.nextToken());
 
-            pqMin.offer(new int[]{P, L});
-            pqMax.offer(new int[]{P, L});
-            map.put(P, L);
-        }
+                // 해당 문제 삭제
+                int l = problemMap.get(p);
 
-        //명령문
-        StringBuffer sb = new StringBuffer();
-        int M = Integer.parseInt(br.readLine());
-        for(int i = 0; i<M; i++){
-            st = new StringTokenizer(br.readLine().trim());
-            String statement = st.nextToken();
-
-            switch(statement){
-                case "recommend":
-                    int num = Integer.parseInt(st.nextToken());
-
-                    if(num == 1){ //가장 어려운 문제 번호 출력
-                        int ans = check(pqMax);
-                        if(ans == 0) break;
-                        
-                        sb.append(ans + "\n");
-                    } else { //가장 쉬운 문제 번호 출력
-                        int ans = check(pqMin);
-                        if(ans == 0) break;
-                        
-                        sb.append(ans + "\n");
-                    }
-                    break;
-
-                case "add":
-                    int P = Integer.parseInt(st.nextToken()); //문제번호
-                    int L = Integer.parseInt(st.nextToken()); //난이도
-
-                    pqMin.offer(new int[]{P, L});
-                    pqMax.offer(new int[]{P, L});
-                    map.put(P, L);
-                    break;
-                    
-                case "solved":
-                    int solved = Integer.parseInt(st.nextToken());
-                    map.remove(solved);
-                    break;
+                problemSet.remove(new Problem(p, l));
+                problemMap.remove(p);
             }
         }
-        System.out.println(sb);
-    }
 
-    static int check(PriorityQueue<int[]> q){ //큐에 들어있는게 실제 있는 문제인지 체크
-        if(!q.isEmpty()) {
-            int[] tmp = q.peek();
-            if(map.containsKey(tmp[0]) && map.get(tmp[0]) == tmp[1]){ //있는 문제면
-                return tmp[0];
-            } else { //없는 문제면
-            	q.poll();
-                return check(q);
-            }
-        }
-		return 0;
+        System.out.print(sb);
     }
 }
