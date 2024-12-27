775bb3a0541f1c4823cf58f6133c0032a8454f76
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2024-12-27 16:37:11 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 468 ms, Memory: 49696 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,75 +3,81 @@ import java.util.*;
 
 public class Main {
 
-	static int V, E;
-	static int[] parents;
+    static int V, E;
+    static List<Edge> list = new ArrayList<>();
+    static int[] parents;
 
-	static void makeSet() {
-		for (int i = 1; i < V + 1; i++) {
-			parents[i] = i;
-		}
-	}
+    public static class Edge implements Comparable<Edge>{
+        int from, to, weight;
 
-	static int find(int a) {
-		if (parents[a] == a)
-			return a;
-		return parents[a] = find(parents[a]);
-	}
+        public Edge(int from, int to, int weight){
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
 
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
+        @Override
+        public int compareTo(Edge o) { //오름차순 정렬
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
 
-		if (aRoot == bRoot)
-			return false;
-		parents[bRoot] = aRoot;
-		return true;
-	}
+    static void makeSet(){
+        for(int i = 1; i<V+1; i++){
+            parents[i] = i;
+        }
+    }
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int find(int a){
+        if(parents[a] == a)
+            return a;
+        return parents[a] = find(parents[a]);
+    }
 
-		int a, b, c;
+    static boolean union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
+        if(aRoot == bRoot){
+            return false;
+        }
+        parents[bRoot] = aRoot;
+        return true;
+    }
 
-		parents = new int[V + 1];
-		makeSet();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		List<int[]> l = new ArrayList<int[]>();
+        V = Integer.parseInt(st.nextToken()); //정점 개수
+        E = Integer.parseInt(st.nextToken()); //간선 개수
 
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken()); // 정점
-			b = Integer.parseInt(st.nextToken()); // 정점
-			c = Integer.parseInt(st.nextToken()); // 가중치
+        parents = new int[V+1];
+        makeSet();
 
-			l.add(new int[] { a, b, c });
-		}
+        for(int i = 0; i<E; i++){
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken()); //가중치
 
-		l.sort(new Comparator<int[]>() {
+            list.add(new Edge(A, B, C));
+        }
 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[2] - o2[2];
-			}
-		});
+        Collections.sort(list); //가중치 기준 오름차순 정렬
 
-		int result = 0;
-		int cnt = 0;
-		for (int[] tt : l) {
-//				System.out.println(Arrays.toString(tt));
-			if (union(tt[0], tt[1])) {
-				result += tt[2];
-				if (++cnt == V - 1)
-					break;
-			}
-		}
+        int result = 0;
+        int cnt = 0;
 
-		System.out.println(result);
+        for(Edge e : list){
+            if(union(e.from, e.to)){
+                result += e.weight;
 
-	}
+                if(++cnt == V-1)
+                    break;
+            }
+        }
 
+        System.out.println(result);
+    }
 }
