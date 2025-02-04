8548871a5e7790e61704d375f52afc361c18523b
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2025-02-04 17:28:56 +0900

    [Gold IV] Title: 네트워크 연결, Time: 408 ms, Memory: 49980 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -2,54 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int N, M;
+    static List<int[]>[] list;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 컴퓨터 수
-        int m = Integer.parseInt(br.readLine()); // 연결할 수 있는 선의 수
+        StringTokenizer st;
 
-        ArrayList<int[]>[] list = new ArrayList[n+1];
-        for(int i = 0; i<n+1; i++){
+        N = Integer.parseInt(br.readLine()); //컴퓨터 수
+        M = Integer.parseInt(br.readLine()); //선의 수
+
+        list = new ArrayList[N+1];
+        for(int i = 1; i<=N; i++){
             list[i] = new ArrayList<>();
         }
 
-        for(int i = 0; i<m ; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            // 각 컴퓨터를 연결하는데 드는 비용
+        for(int i = 0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken()); //비용
+            int c = Integer.parseInt(st.nextToken()); //a,b 연결하는데 드는 비용
 
-            list[a].add(new int[]{b,cost});
-            list[b].add(new int[]{a,cost});
+            list[a].add(new int[]{b, c});
+            list[b].add(new int[]{a, c});
         }
-
-        //모든 컴퓨터를 연결하는데 필요한 최소 비용
-        boolean[] visited = new boolean[n+1];
-        int sum = 0;
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>(){
             @Override
             public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[1], o2[1]);
+                return Integer.compare(o1[1], o2[1]); //비용 적은 순서
             }
         });
 
+        boolean[] visited = new boolean[N+1];
+        int result = 0;
         pq.add(new int[]{1,0});
-
+        
         while(!pq.isEmpty()){
             int[] cur = pq.poll();
 
             if(!visited[cur[0]]){
                 visited[cur[0]] = true;
-                sum += cur[1];
+                result += cur[1];
 
-                for(int j = 0; j<list[cur[0]].size(); j++){
-                    if(!visited[list[cur[0]].get(j)[0]]){
-                        pq.add(list[cur[0]].get(j));
+                for(int i = 0; i<list[cur[0]].size(); i++){
+                    if(!visited[list[cur[0]].get(i)[0]]){
+                        pq.add(list[cur[0]].get(i));
                     }
                 }
             }
         }
-        System.out.println(sum);
+
+        System.out.println(result);
     }
 }
