862be7814cf408ca08bfbee6e6e9a8ff88d236f5
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2025-04-02 18:30:17 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 188 ms, Memory: 13628 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.java
@@ -1,59 +1,61 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
 
-	static int N;
-	static int[][] egg;
-	static int max = Integer.MIN_VALUE;
+    static int N;
+    static int[][] egg;
+    static int result = Integer.MIN_VALUE;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); // 계란수
-		egg = new int[N][2];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			egg[i][0] = Integer.parseInt(st.nextToken()); // 내구도
-			egg[i][1] = Integer.parseInt(st.nextToken()); // 무게
-		}
-		if (N == 1) {
-			max = 0;
-		} else {
-			dfs(0, 0);
-		}
-		System.out.println(max);
-	}
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	static void dfs(int idx, int cnt) {
-		if (idx == N) {
-			if (cnt > max) {
-				max = cnt;
-			}
-			return;
-		}
+        N = Integer.parseInt(br.readLine()); //계란
+        egg = new int[N][2];
 
-		if (egg[idx][0] <= 0 || cnt == N-1) {// 해당 계란 내구도 0이면 넘어가기
-			dfs(idx+1, cnt);
-			return;
-		}
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            egg[i][0] = Integer.parseInt(st.nextToken()); //내구도
+            egg[i][1] = Integer.parseInt(st.nextToken()); //무게
+        }
 
-		for (int i = 0; i < N; i++) {
-			if (idx == i || egg[i][0] <= 0) continue;
+        if (N == 1) {
+            result = 0;
+        } else {
+            dfs(0, 0);
+        }
+        System.out.println(result);
+    }
 
-			egg[idx][0] -= egg[i][1];
-			egg[i][0] -= egg[idx][1];
+    static void dfs(int idx, int cnt) {
+        if (idx == N) {
+            if (cnt > result) {
+                result = cnt;
+            }
+            return;
+        }
 
-			if (egg[idx][0] <= 0) cnt++;
-			if (egg[i][0] <= 0) cnt++;
+        if (egg[idx][0] <= 0 || cnt == N-1) {// 해당 계란 내구도 0일 때
+            dfs(idx+1, cnt);
+            return;
+        }
 
-			dfs(idx + 1, cnt);
-			
-			if (egg[idx][0] <= 0) cnt--;
-			if (egg[i][0] <= 0) cnt--;
+        for (int i = 0; i < N; i++) {
+            if (idx == i || egg[i][0] <= 0) continue;
 
-			egg[idx][0] += egg[i][1];
-			egg[i][0] += egg[idx][1];
+            egg[idx][0] -= egg[i][1];
+            egg[i][0] -= egg[idx][1];
 
-		}
-	}
+            if (egg[idx][0] <= 0) cnt++;
+            if (egg[i][0] <= 0) cnt++;
+
+            dfs(idx + 1, cnt);
+
+            if (egg[idx][0] <= 0) cnt--;
+            if (egg[i][0] <= 0) cnt--;
+
+            egg[idx][0] += egg[i][1];
+            egg[i][0] += egg[idx][1];
+        }
+    }
 }
