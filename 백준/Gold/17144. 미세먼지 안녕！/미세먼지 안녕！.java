937b34ac334f85d907b502fa1dad1fddc4695d13
HeewonYoun <59885440+HeewonYoun@users.noreply.github.com>
2025-05-08 18:24:46 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 468 ms, Memory: 166404 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -3,133 +3,100 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int R = Integer.parseInt(st.nextToken()); //행
-		int C = Integer.parseInt(st.nextToken()); //열
-		int T = Integer.parseInt(st.nextToken()); //T초
-		
-		int[][] map = new int[R][C];
-		Queue<int[]> q = new ArrayDeque<int[]>();
-		List<int[]> air = new ArrayList<int[]>();
-	
-		for(int i = 0; i<R; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j<C; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				
-				//미세먼지
-				if(map[i][j] > 1) {
-					q.offer(new int[] {i,j,map[i][j]});
-				}
-				
-				//공기청정기 위, 아래
-				if(map[i][j] == -1) {
-					air.add(new int[] {i,j});
-				}
-			}
-		}
-		
-		air.sort(new Comparator<int[]>() { //위, 아래 .. 
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[0] - o2[0];
-			}
-		});
-		
-		int[] dx = {-1,1,0,0}; //상하좌우
-		int[] dy = {0,0,-1,1}; 
-		
-		for(int i = 0; i<T; i++) {
+    static int R, C, T;
+    static int[][] map;
+    static Queue<int[]> q; //미세먼지
+    static List<int[]> air = new ArrayList<>(); //공기청정기
 
-			//미세먼지 확산
-			while(!q.isEmpty()) {
-				int[] cur = q.poll();
-				int A = cur[2]; //확산 모든 칸에서 동시에 
-				
-				int cnt = 0; //확산된 방향의 개수
-				for(int j = 0; j<4; j++) {
-					int nx = cur[0] + dx[j];
-					int ny = cur[1] + dy[j];
-					
-					//인접방향 공기청정기, 칸이 없으면 확산X
-					if(nx < 0 || nx >= R || ny < 0 || ny >= C || map[nx][ny] == -1) continue;
+    static int[] dx = {-1, 1, 0, 0}; // 상하좌우
+    static int[] dy = {0, 0, -1, 1};
 
-					cnt++;
-					map[nx][ny] += (int)(A/5);
-					
-				}
-				
-				//4방향 돌고 해당 위치 남은 미세먼지 양 바꾸기 A - (A/5)*확산방향 개수
-				map[cur[0]][cur[1]] = map[cur[0]][cur[1]] - (A/5)*cnt;
-			}
-			
-			//공기청정기 작동
-			//윗부분 돌리기
-			int[] cur = air.get(0);
-			
-			for(int j = cur[0]; j>0; j--) {
-				if(j == cur[0]) map[j-1][0] = 0;
-				else map[j][0] = map[j-1][0];
-			}
-			
-			for(int j = 0; j<C-1; j++) {
-				map[0][j] = map[0][j+1];
-			}
-			
-			for(int j = 0; j<cur[0]; j++) {
-				map[j][C-1] = map[j+1][C-1];
-			}
-			
-			for(int j = C-1; j>0; j--) {
-				if(j == 1) map[cur[0]][j] = 0;
-				else map[cur[0]][j] = map[cur[0]][j-1];
-			}
-			
-			///////////////////////////////////
-			//아랫부분 돌리기
-			cur = air.get(1);
-			
-			for(int j = cur[0]; j<R-1; j++) {
-				if(j == cur[0]) map[j+1][0] = 0;
-				else map[j][0] = map[j+1][0];
-			}
-			
-			for(int j = 0; j<C-1; j++) {
-				map[R-1][j] = map[R-1][j+1];
-			}
-			
-			for(int j = R-1; j>cur[0]; j--) {
-				map[j][C-1] = map[j-1][C-1];
-			}
-			
-			for(int j = C-1; j>0; j--) {
-				if(j == 1) map[cur[0]][j] = 0;
-				else map[cur[0]][j] = map[cur[0]][j-1];
-			}
-			
-			//다음 확산 전에 queue에 넣기
-			for(int j = 0; j<R; j++) {
-				for(int k = 0; k<C; k++) {
-					if(map[j][k] > 1) {
-						q.offer(new int[] {j,k,map[j][k]});
-					}
-				}
-			}	
-		}
-		
-		//방에 남아있는 미세먼지 양
-		int result = 2;
-		for(int i = 0; i<R; i++) {
-			for(int j = 0; j<C; j++) {
-				result += map[i][j];
-			}
-		}
-		
-		System.out.println(result);
-		
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+
+        map = new int[R][C];
+        q = new LinkedList<>();
+
+        for(int i = 0; i < R; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < C; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if(map[i][j] > 0) q.offer(new int[]{i, j, map[i][j]});
+                else if(map[i][j] == -1) air.add(new int[]{i, j});
+            }
+        }
+
+        for(int t = 0; t < T; t++){
+            spread(); //미세먼지 확신
+            operateAirCleaner();
+
+            q.clear();
+            for(int i = 0; i < R; i++){
+                for(int j = 0; j < C; j++){
+                    if(map[i][j] > 0) q.offer(new int[]{i, j, map[i][j]});
+                }
+            }
+        }
+
+        int result = 0;
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < C; j++){
+                if(map[i][j] > 0) result += map[i][j];
+            }
+        }
+        System.out.println(result);
+    }
+
+    static void spread(){
+        int[][] temp = new int[R][C];
+
+        for(int[] pos : air){
+            temp[pos[0]][pos[1]] = -1;
+        }
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1], amount = cur[2];
+            int count = 0; //확산된 방향수
+
+            for(int d = 0; d < 4; d++){
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+
+                if(nx < 0 || nx >= R || ny < 0 || ny >= C || map[nx][ny] == -1) continue;
+
+                temp[nx][ny] += amount / 5;
+                count++;
+            }
+
+            temp[x][y] += amount - (amount / 5 * count);
+        }
+
+        map = temp;
+    }
+
+    static void operateAirCleaner(){ //공기청정기 작동
+        int upper = air.get(0)[0];
+        int lower = air.get(1)[0];
+
+        // 위쪽 반시계방향
+        for(int i = upper - 1; i > 0; i--) map[i][0] = map[i-1][0];
+        for(int i = 0; i < C - 1; i++) map[0][i] = map[0][i+1];
+        for(int i = 0; i < upper; i++) map[i][C-1] = map[i+1][C-1];
+        for(int i = C - 1; i > 1; i--) map[upper][i] = map[upper][i-1];
+        map[upper][1] = 0;
+
+        // 아래쪽 시계방향
+        for(int i = lower + 1; i < R - 1; i++) map[i][0] = map[i+1][0];
+        for(int i = 0; i < C - 1; i++) map[R-1][i] = map[R-1][i+1];
+        for(int i = R - 1; i > lower; i--) map[i][C-1] = map[i-1][C-1];
+        for(int i = C - 1; i > 1; i--) map[lower][i] = map[lower][i-1];
+        map[lower][1] = 0;
+    }
 }
